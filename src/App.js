b8daed24ca6b233ab890cc8a1a34cb74788ba3ce
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-04-11 14:12:48 -0600

    final push to github

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import Container from "./components/Container";
-import SearchForm from "./components/searchForm";
-import SearchResults from "./components/SearchResults";
-import Footer from "./components/Footer";
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Table from "./components/Table"
+import { EmployeeAttributes } from "./components/EmployeeAttributes"
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [name, setName] = useState("");
-
-  const handleInputChange = event => {
-    setSearch(event.target.value);
-  };
-
-  const handleFormSubmit = event => {
-    event.prevenDefault();
-  };
+  const [employees, allEmployees] = useState([]);
+  const [criteriaEmployees, visibleEmployees] = useState([])
 
   return (
-    <div>
-      <Container>
-        <h1 className="text-center">Employee Dashboard</h1>
-        <SearchForm
-          handleFormSubmit={handleFormSubmit}
-          hadleInputChange={handleInputChange}
-          results={search}
-        />
-        <SearchResults />
-      </Container>
-      <Footer />
+    <div className="App">
+      <EmployeeAttributes.Provider value={{ employees, allEmployees, criteriaEmployees, visibleEmployees }}>
+        <Navbar />
+        <Table />
+      </EmployeeAttributes.Provider>
     </div>
   );
 }
