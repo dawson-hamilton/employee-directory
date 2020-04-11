36716e5efed3a6184f7b150e5bfb6ffc21fff0bc
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-04-11 14:28:25 -0600

    trying to deploy with github pages

---
+++ src/components/TableRow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TableRow() {
+    return (
+        <tr>
+            <td>Dawson</td>
+            <td>Hamilton</td>
+            <td>0982922</td>
+            <td>Male</td>
+            <td>dawsondhamilton@gmail.com</td>
+            <td>20</td>
+        </tr>
+    )
+}
+export default TableRow;
